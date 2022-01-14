88dfa61f4f2e2c3129b54ea1a9b277c3175ae82f
Asakura23 <91260167+Asakura23@users.noreply.github.com>
2022-01-14 13:39:34 +0300

    Add files via upload

--- tests/_support/AcceptanceTester.php
+++ tests/_support/AcceptanceTester.php
@@ -1,4 +1,8 @@
 <?php
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\Remote\RemoteWebElement;
+
 
 
 /**
@@ -20,7 +24,60 @@ class AcceptanceTester extends \Codeception\Actor
 {
     use _generated\AcceptanceTesterActions;
 
-    /**
-     * Define custom actions here
-     */
+
+    use _generated\AcceptanceTesterActions;
+
+    public function haveVisible($element)
+    {
+        $I = $this;
+        $value = false;
+        $I->executeInSelenium(function (\Facebook\WebDriver\Remote\RemoteWebDriver $webDriver) use ($element, &$value) {
+
+            $element = $webDriver->findElement(WebDriverBy::cssSelector($element));
+            codecept_debug(get_class($element));
+            $value = $element instanceof RemoteWebElement;
+
+        });
+        return $value;
+
+    }
+
+    public function checkVisible($element)
+    {
+
+        $I = $this;
+
+        ($vp = $I->haveVisible($element));
+        if (!$vp = 1) {
+
+            codecept_debug("$vp sssss");
+            codecept_debug($this->haveVisible($element));
+            codecept_debug($element);
+
+
+            PHPUnit\Framework\Assert::fail();
+        }
+    }
+
+    public function loginPortal()
+    {
+        $I = $this;
+        $I->wantTo('check');
+        $ini_array = parse_ini_file("Dat.ini");
+        $I->amOnPage('/');
+        $login = $ini_array["Login"];
+        $pass = $ini_array["Pass"];
+        $captcha = $ini_array["Captcha"];
+        $sms = $ini_array["Sms"];
+        $I->fillField('Логин *', $login);
+        $I->fillField('Пароль *', $pass);
+        $I->fillField('Код проверки *', $captcha);
+        $I->click('Войти');
+        $I->waitForText("Смс код", 10);
+        $I->fillField('Смс код *', $sms);
+        $I->click('Войти');
+        $I->waitForText("Личный кабинет", 10);
+    }
 }
+
+
