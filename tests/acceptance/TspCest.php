188cc411b47a562839d78c5e1021500630229366
Asakura23 <91260167+Asakura23@users.noreply.github.com>
2022-01-14 21:00:44 +0300

    Add files via upload

--- tests/acceptance/TspCest.php
+++ tests/acceptance/TspCest.php
@@ -10,13 +10,13 @@ class tspCest
     // tests
     public function tryToTest(AcceptanceTester $I)
     {
-        $I->wantTo("check p.8");
+        $I->wantTo("Проверить в поле ТСП что в списке больше одной опции (и что есть опция Все)");
         $I->loginPortal();
         $I->click(['css'=>'.item:nth-child(2) .drop:nth-child(3)']);
         $I->wait(3);
         $I->see("Все",['css'=>'.item:nth-child(2) .MS_drop:last-child']);
         $I->see("22000001 - 22000001 (Москва)",['css'=>'.item:nth-child(2) .MS_drop:last-child']);
-        $I->wantTo("check p.10");
+        $I->wantTo("Проверить в поле ТСП Снять галку с \"Все\", остальные выбираются (причем работает множественный выбор)");
         $I->click(['css'=>'.item:nth-child(2) li:first-child span']);
         $I->wait(1);
         $I->click(['css'=>'.item:nth-child(2) li:first-child span']);
