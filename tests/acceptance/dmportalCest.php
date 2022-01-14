099f8b214b8ee67621d5cacaaeecc5cfbcd9e95c
Asakura23 <91260167+Asakura23@users.noreply.github.com>
2022-01-14 13:40:10 +0300

    Add files via upload

--- tests/acceptance/dmportalCest.php
+++ tests/acceptance/dmportalCest.php
@@ -1,22 +1,18 @@
 <?php
-
-class DmportalCest
+use Facebook\WebDriver\Remote\RemoteWebDriver;
+use Facebook\WebDriver\WebDriverBy;
+use Facebook\WebDriver\Remote\RemoteWebElement;
+class dmportalCest
 {
     public function _before(AcceptanceTester $I)
     {
-    }
-
-    // tests
-    public function tryToTest(AcceptanceTester $I)
-    {
         $I->wantTo('check');
-        $ini_array= parse_ini_file("Dat.ini");
+        $ini_array = parse_ini_file("Dat.ini");
         $I->amOnPage('/');
-        codecept_debug($ini_array);
-        $login=$ini_array["Login"];
-        $pass=$ini_array["Pass"];
-        $captcha=$ini_array["Captcha"];
-        $sms=$ini_array["Sms"];
+        $login = $ini_array["Login"];
+        $pass = $ini_array["Pass"];
+        $captcha = $ini_array["Captcha"];
+        $sms = $ini_array["Sms"];
         $I->fillField('Логин *', $login);
         $I->fillField('Пароль *', $pass);
         $I->fillField('Код проверки *', $captcha);
@@ -25,8 +21,15 @@ class DmportalCest
         $I->fillField('Смс код *', $sms);
         $I->click('Войти');
         $I->waitForText("Личный кабинет", 10);
+    }
+
+
+    public function tryToTest(AcceptanceTester $I)
+    {
+
         $I->click('viewOperations');
-
-
+        $I->waitForText("Экспорт",10);
+        $I->checkVisible(".viewOperations");
+        $I->DontSeeElement(".error");
     }
 }
